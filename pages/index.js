936ef0963afce78b8a19f28ebfa8f4845538fb9d
lolieatapple <lolieatapple@gmail.com>
2020-05-12 23:55:49 +0800

    update ui.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import { Component } from "../components/base";
-import { Row, Col, Input, Slider, Radio, Table } from "antd";
+import { Row, Col, Input, Slider, Radio, Table, Button } from "antd";
 import BigNumber from 'bignumber.js';
 import { Wallet, getSelectedAccount, WalletButton, WalletButtonLong, getSelectedAccountWallet, getTransactionReceipt } from "wan-dex-sdk-wallet";
 import "wan-dex-sdk-wallet/index.css";
@@ -77,6 +77,50 @@ class IndexPage extends Component {
             </Col>
           </Row>
           <Table />
+          <div className={styles.center}>
+            <Button type="primary" style={{margin: "20px"}}>Hedge Now</Button>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <h1>Leverage your BTC</h1>
+          <Row gutter={[16, 16]} className={styles.center}>
+            <Col span={4}>How many BTC are you holding</Col>
+            <Col span={4}>Expiration</Col>
+            <Col span={3}>Currency to pay</Col>
+            <Col span={6}>Hedge Price in $ / choosed currency</Col>
+            <Col span={6}>Risk due to the price go down in percentage</Col>
+          </Row>
+          <Row gutter={[16, 16]} className={styles.center}>
+            <Col span={4}>
+              <Row gutter={[16, 16]}>
+                <Col span={12}><Input /></Col>
+                <Col span={12}><Slider /></Col>
+              </Row>
+            </Col>
+            <Col span={4}>
+              <Radio.Group defaultValue="a" buttonStyle="solid">
+                <Radio.Button value="a">15/May</Radio.Button>
+                <Radio.Button value="b">30/May</Radio.Button>
+              </Radio.Group>
+            </Col>
+            <Col span={3}>
+              <Radio.Group defaultValue="a" buttonStyle="solid">
+                <Radio.Button value="a">WAN</Radio.Button>
+                <Radio.Button value="b">FNX</Radio.Button>
+              </Radio.Group>
+            </Col>
+            <Col span={6}>$100 / WAN 1000</Col>
+            <Col span={6}>
+              <Row gutter={[16, 16]}>
+                <Col span={8}>$1,100</Col>
+                <Col span={16}><Slider /></Col>
+              </Row>
+            </Col>
+          </Row>
+          <Table />
+          <div className={styles.center}>
+            <Button type="primary" style={{margin: "20px"}}>Hedge Now</Button>
+          </div>
         </div>
         <div></div>
         <div></div>

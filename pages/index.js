79b31ce7ade5662fded970289f5557225ef79e5b
lolieatapple <lolieatapple@gmail.com>
2020-05-14 00:07:11 +0800

    simple ui finish.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import { Component } from "../components/base";
-import { Row, Col, Input, Slider, Radio, Table, Button } from "antd";
+import { Row, Col, Input, Slider, Radio, Table, Button, Divider } from "antd";
 import BigNumber from 'bignumber.js';
 import { Wallet, getSelectedAccount, WalletButton, WalletButtonLong, getSelectedAccountWallet, getTransactionReceipt } from "wan-dex-sdk-wallet";
 import "wan-dex-sdk-wallet/index.css";
@@ -37,6 +37,147 @@ class IndexPage extends Component {
 
   }
 
+  hedgeColumn = [
+    {
+      title: 'Token name',
+      dataIndex: "tokenName",
+      key: 'tokenName',
+    },
+    {
+      title:'Underlying assets price',
+      dataIndex:'underlyingAssetsPrice',
+      key:'underlyingAssetsPrice',
+    },
+    {
+      title:'Expiration',
+      dataIndex:'expiration',
+      key:'expiration',
+    },
+    {
+      title:'Strike price',
+      dataIndex:'strikePrice',
+      key:'strikePrice',
+    },
+    {
+      title:'Liquidity',
+      dataIndex:'liquidity',
+      key:'liquidity',
+    },
+    {
+      title:'Price',
+      dataIndex:'price',
+      key:'price',
+    },
+    {
+      title:'Percentage of collateral',
+      dataIndex:'percentageOfCollateral',
+      key:'percentageOfCollateral',
+    },
+  ]
+
+  leverageColumn = [
+
+  ]
+
+  myAssetsColumn = [
+    {
+      title: 'Token name',
+      dataIndex: "tokenName",
+      key: 'tokenName',
+    },
+    {
+      title:'Underlying assets price',
+      dataIndex:'underlyingAssetsPrice',
+      key:'underlyingAssetsPrice',
+    },
+    {
+      title:'Strike price',
+      dataIndex:'strikePrice',
+      key:'strikePrice',
+    },
+    {
+      title:'Amount',
+      dataIndex:'amount',
+      key:'amount',
+    },
+    {
+      title:'Price paid',
+      dataIndex:'pricePaid',
+      key:'pricePaid',
+    },
+    {
+      title:'Price now',
+      dataIndex:'priceNow',
+      key:'priceNow',
+    },
+    {
+      title:'Percentage of collateral',
+      dataIndex:'percentageOfCollateral',
+      key:'percentageOfCollateral',
+    },
+    {
+      title:'Expected Return',
+      dataIndex:'expectedReturn',
+      key:'expectedReturn',
+    },
+    {
+      title:'',
+      dataIndex:'',
+      key:'action',
+      render: () => {
+        return(<Button type="primary">Sell now</Button>)
+      }
+    },
+  ]
+
+  hedgeDemo = [
+    {
+      tokenName:'BTC call, 9th 5, $7000',
+      underlyingAssetsPrice:'$7200',
+      expiration:'9th 5',
+      strikePrice:'$7000',
+      liquidity:'20',
+      price:'$60',
+      percentageOfCollateral:'130%',
+    }
+  ]
+
+  leverageDemo = [
+    {
+      tokenName:'BTC put, 9th 5, $7000',
+      underlyingAssetsPrice:'$7200',
+      expiration:'9th 5',
+      strikePrice:'$7000',
+      liquidity:'20',
+      price:'$60',
+      percentageOfCollateral:'130%',
+    }
+  ]
+
+  myAssetDemo = [
+    {
+      tokenName:'BTC call, 9th 5, $7000',
+      underlyingAssetsPrice:'$7200',
+      strikePrice:'$7000',
+      amount:'20',
+      pricePaid:'$60',
+      priceNow:'$180',
+      percentageOfCollateral:'130%',
+      expectedReturn: '$200'
+    },
+    {
+      tokenName:'BTC put, 9th 5, $7000',
+      underlyingAssetsPrice:'$7200',
+      strikePrice:'$7000',
+      amount:'30',
+      pricePaid:'$60',
+      priceNow:'$80',
+      percentageOfCollateral:'120%',
+      expectedReturn: '$0'
+    }
+
+  ]
+
   render() {
     return (
       <div>
@@ -46,7 +187,7 @@ class IndexPage extends Component {
             <Col span={4}>How many BTC are you holding</Col>
             <Col span={4}>Expiration</Col>
             <Col span={3}>Currency to pay</Col>
-            <Col span={6}>Hedge Price in $ / choosed currency</Col>
+            <Col span={6}>Hedge Price in $ / chose currency</Col>
             <Col span={6}>Risk due to the price go down in percentage</Col>
           </Row>
           <Row gutter={[16, 16]} className={styles.center}>
@@ -76,19 +217,20 @@ class IndexPage extends Component {
               </Row>
             </Col>
           </Row>
-          <Table />
+          <Table dataSource={this.hedgeDemo} columns={this.hedgeColumn} />
           <div className={styles.center}>
             <Button type="primary" style={{margin: "20px"}}>Hedge Now</Button>
           </div>
         </div>
+        <Divider/>
         <div className={styles.box}>
           <h1>Leverage your BTC</h1>
           <Row gutter={[16, 16]} className={styles.center}>
-            <Col span={4}>How many BTC are you holding</Col>
+            <Col span={4}>How many BTC are you plan to leverage</Col>
             <Col span={4}>Expiration</Col>
             <Col span={3}>Currency to pay</Col>
-            <Col span={6}>Hedge Price in $ / choosed currency</Col>
-            <Col span={6}>Risk due to the price go down in percentage</Col>
+            <Col span={6}>Leverage Price in $ / chose currency</Col>
+            <Col span={6}>Return due to the price go up in percentage</Col>
           </Row>
           <Row gutter={[16, 16]} className={styles.center}>
             <Col span={4}>
@@ -117,12 +259,16 @@ class IndexPage extends Component {
               </Row>
             </Col>
           </Row>
-          <Table />
+          <Table dataSource={this.leverageDemo} columns={this.hedgeColumn}/>
           <div className={styles.center}>
-            <Button type="primary" style={{margin: "20px"}}>Hedge Now</Button>
+            <Button type="primary" style={{margin: "20px"}}>Leverage Now</Button>
           </div>
         </div>
-        <div></div>
+        <Divider/>
+        <div className={styles.box}>
+          <h1>My assets</h1>
+          <Table dataSource={this.myAssetDemo} columns={this.myAssetsColumn}/>
+        </div>
         <div></div>
       </div>
     );

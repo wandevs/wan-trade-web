8d73b499373710f8a9586a1575d07dec8b205c15
lolieatapple <lolieatapple@gmail.com>
2020-07-08 11:31:10 +0800

    update mainnet tokens.

--- conf/config.js
+++ conf/config.js
@@ -6,6 +6,6 @@ export const mainnetProxyAddr = '0xff6d4cca7509573faa92013496399b82760cf269'; //
 export const testnetProxyAddr = '0x9e57b9f1d836ff1701e441a619cbaad7fc8863d3'; //testnet
 
 // change networkId to switch network
-export const networkId = 3; //1:mainnet, 3:testnet;
+export const networkId = 1; //1:mainnet, 3:testnet;
 
 export const nodeUrl = networkId == 1 ? "wss://api.wanchain.org:8443/ws/v3/2d044687a34aa9bf2eee7ab4fe61044f7962bb06e188ddaaedfd45f793fab837" : "wss://apitest.wanchain.org:8443/ws/v3/2d044687a34aa9bf2eee7ab4fe61044f7962bb06e188ddaaedfd45f793fab837";
